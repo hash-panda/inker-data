d843f3a9ce051acd36d10230870a2cb658cee7e5
fomoFi.icu <joneclub1990@gmail.com>
2022-02-21 00:16:15 +0800

    tips 修改

--- src/pages/dashboard/locale/zh-CN.ts
+++ src/pages/dashboard/locale/zh-CN.ts
@@ -13,10 +13,10 @@ export default {
     '存款金额大于11ust的玩家数量（官方文档明确最小存款金额为11ust）',
   'dashboard.dataPanel.playerAverageDepositAmount': '玩家平均存款金额',
   'dashboard.dataPanel.playerAverageDepositAmount.tips':
-    '玩家平均存款金额 = (总存款金额 - 赞助商的存款金额) / 总玩家数',
+    '玩家平均存款金额 = (总存款金额 - 赞助商的存款金额) / 有效玩家数',
   'dashboard.dataPanel.averageDepositAmount': '平均存款金额',
   'dashboard.dataPanel.averageDepositAmount.tips':
-    '平均存款金额 = (总存款金额) / (有效玩家数)',
+    '平均存款金额 = (总存款金额) / (总玩家数)',
   'dashboard.tips':
     '说明：[50,100)代表存款金额大于等于 50ust 小于 100ust 的玩家的总存款金额和总人数',
 };
