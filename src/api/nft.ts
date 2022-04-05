3be15165923f50f05bf5cc1b9f47effb7da23666
fomoFi.icu <joneclub1990@gmail.com>
2022-04-05 15:45:33 +0800

    增加接口

--- src/api/nft.ts
+++ src/api/nft.ts
@@ -1,5 +1,25 @@
 import axios from 'axios';
 
+
+// https://fcd.terra.dev/wasm/contracts/terra14f5y8j5udr48a3prakm3j8st96u3rczuqtlc55/store?query_msg=%7B%22all_nft_info%22:%7B%22token_id%22:%222501%22%7D%7D
+export function queryTokenIdByAddress(address: string) {
+  const queryMsg = {
+    tokens: {
+      owner: address,
+    },
+  };
+  const encodeQueryMsg = JSON.stringify(queryMsg);
+  const requestParams = {
+    params: {
+      query_msg: encodeQueryMsg,
+    },
+  };
+  return axios.get<any>(
+    'https://fcd.terra.dev/wasm/contracts/terra14f5y8j5udr48a3prakm3j8st96u3rczuqtlc55/store',
+    requestParams
+  );
+}
+
 // https://fcd.terra.dev/wasm/contracts/terra14f5y8j5udr48a3prakm3j8st96u3rczuqtlc55/store?query_msg=%7B%22all_nft_info%22:%7B%22token_id%22:%222501%22%7D%7D
 export function queryNftList(tokenId: string) {
   const queryMsg = {
