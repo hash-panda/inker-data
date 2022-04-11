634b0fbc04361fac80817f3ffe6bcd59494df99d
fomoFi.icu <joneclub1990@gmail.com>
2022-04-11 22:39:55 +0800

    update rarity nft rank

--- src/store/modules/nft/index.ts
+++ src/store/modules/nft/index.ts
@@ -1,60 +1,60 @@
-import { defineStore, acceptHMRUpdate } from 'pinia'
-import { tigerNftList } from './tigerNftList'
-import { tigerNftRarity } from './tigerNftRarity'
+import { defineStore, acceptHMRUpdate } from 'pinia';
+import { tigerNftList } from './tigerNftList';
+import { tigerNftRarity } from './tigerNftRarity';
 
 export const useNftState = defineStore('nftState', {
-    state: () => {
-        return {
-            tigerNftList: [],
-            tigerNftRarityRank: [],
-            // tigerNftRarityRank: import.meta.env.MODE === 'development' ? [] : tigerNftRarity,
+  state: () => {
+    return {
+      tigerNftList: [],
+      tigerNftRarityRank: tigerNftRarity,
+      // tigerNftRarityRank: import.meta.env.MODE === 'development' ? [] : tigerNftRarity,
+    };
+  },
+  getters: {
+    totalNft(state) {
+      return state.tigerNftList.length;
+    },
+    tigerNftPagenationList(state) {
+      const nftList = [] as any;
+      state.tigerNftList.forEach((item: any, index: number) => {
+        const page = Math.floor(index / 50);
+        if (!nftList[page]) {
+          nftList[page] = [];
         }
+        nftList[page].push(item);
+      });
+      return nftList;
     },
-    getters: {
-        totalNft(state) {
-            return state.tigerNftList.length
-        },
-        tigerNftPagenationList(state) {
-            const nftList = [] as any
-            state.tigerNftList.forEach((item: any, index: number) => {
-                const page = Math.floor(index / 50)
-                if (!nftList[page]) {
-                    nftList[page] = []
-                }
-                nftList[page].push(item)
-            })
-            return nftList
-        },
-        // tigerNftRarityRankPagenationList(state) {
-        //   const nftList = [] as any;
-        //   state.tigerNftRarityRank.forEach((item: any, index: number) => {
-        //     const page = Math.floor(index / 50);
-        //     if (!nftList[page]) {
-        //       nftList[page] = [];
-        //     }
-        //     nftList[page].push(item);
-        //   });
-        //   return nftList;
-        // },
-    },
-    actions: {},
-    // persist: {
-    //   enabled: true,
-    //   strategies: [
-    //     {
-    //       key: 'inker_tiger_nft_list',
-    //       storage: sessionStorage,
-    //       paths: ['tigerNftList'],
-    //     },
-    //     {
-    //       key: 'inker_tiger_nft_rank_list',
-    //       storage: sessionStorage,
-    //       paths: ['tigerNftRarityRank'],
+    // tigerNftRarityRankPagenationList(state) {
+    //   const nftList = [] as any;
+    //   state.tigerNftRarityRank.forEach((item: any, index: number) => {
+    //     const page = Math.floor(index / 50);
+    //     if (!nftList[page]) {
+    //       nftList[page] = [];
     //     }
-    //   ],
+    //     nftList[page].push(item);
+    //   });
+    //   return nftList;
     // },
-})
+  },
+  actions: {},
+  // persist: {
+  //   enabled: true,
+  //   strategies: [
+  //     {
+  //       key: 'inker_tiger_nft_list',
+  //       storage: sessionStorage,
+  //       paths: ['tigerNftList'],
+  //     },
+  //     {
+  //       key: 'inker_tiger_nft_rank_list',
+  //       storage: sessionStorage,
+  //       paths: ['tigerNftRarityRank'],
+  //     }
+  //   ],
+  // },
+});
 
 if (import.meta.hot) {
-    import.meta.hot.accept(acceptHMRUpdate(useNftState, import.meta.hot))
+  import.meta.hot.accept(acceptHMRUpdate(useNftState, import.meta.hot));
 }
