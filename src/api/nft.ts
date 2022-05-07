599f3a7158a433370b5618114d887c6567dfa01d
fomoFi.icu <95140885+fomofi@users.noreply.github.com>
2022-05-07 18:49:30 +0800

    孔雀NFT

--- src/api/nft.ts
+++ src/api/nft.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 // https://fcd.terra.dev/wasm/contracts/terra14f5y8j5udr48a3prakm3j8st96u3rczuqtlc55/store?query_msg=%7B%22all_nft_info%22:%7B%22token_id%22:%222501%22%7D%7D
 export function queryTokenIdByAddress(address: string) {
   const queryMsg = {
@@ -50,6 +49,24 @@ export function queryTigerNftsFromKnowhere() {
     traits: [],
     query: '',
     nftContract: 'terra14f5y8j5udr48a3prakm3j8st96u3rczuqtlc55',
+  };
+  return axios.post<any>(
+    'https://production-backend-v3.knowhere.art/nfts',
+    requestParams
+  );
+}
+
+export function queryProsperNftsFromKnowhere() {
+  const requestParams = {
+    limit: 3000,
+    offset: 0,
+    saleStatuses: ['NotStarted', 'InProgress'],
+    saleTypes: [],
+    sortBy: 'Price',
+    sortType: 'ASC',
+    traits: [],
+    query: '',
+    nftContract: 'terra1qd2djwhrc9prwztsf6t6necs9f5scdwksvs6ad',
   };
   return axios.post<any>(
     'https://production-backend-v3.knowhere.art/nfts',
